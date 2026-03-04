d94970c8134847aeda9b87a84b52a8b80efdf141
matiascshaha <matias.perichon@hotmail.com>
2026-03-04 17:47:38 -0500

    tests: move yahoo quote fake out of test module for purity

--- tests/unit/test_yahoo_quote_provider.py
+++ tests/unit/test_yahoo_quote_provider.py
@@ -1,20 +1,14 @@
 import pytest
 
 from src.market_data.yahoo.quote_provider import YahooQuoteProvider
+from tests.support.fakes.yahoo_quote_provider import build_fake_yahoo_ticker
 
 
 pytestmark = [pytest.mark.unit]
 
-
-class _FakeTicker:
-    def __init__(self, fast_info=None, info=None):
-        self.fast_info = fast_info if fast_info is not None else {}
-        self.info = info if info is not None else {}
-
-
 def test_yahoo_quote_provider_prefers_ask_for_buy_side():
     provider = YahooQuoteProvider(
-        ticker_factory=lambda _: _FakeTicker(fast_info={"ask": 101.25, "bid": 101.10})
+        ticker_factory=lambda _: build_fake_yahoo_ticker(fast_info={"ask": 101.25, "bid": 101.10})
     )
 
     price = provider.get_limit_reference_price("AAPL", "BUY")
@@ -24,7 +18,7 @@ def test_yahoo_quote_provider_prefers_ask_for_buy_side():
 
 def test_yahoo_quote_provider_prefers_bid_for_sell_side():
     provider = YahooQuoteProvider(
-        ticker_factory=lambda _: _FakeTicker(fast_info={"ask": 101.25, "bid": 101.10})
+        ticker_factory=lambda _: build_fake_yahoo_ticker(fast_info={"ask": 101.25, "bid": 101.10})
     )
 
     price = provider.get_limit_reference_price("AAPL", "SELL")
@@ -34,7 +28,7 @@ def test_yahoo_quote_provider_prefers_bid_for_sell_side():
 
 def test_yahoo_quote_provider_falls_back_to_info_when_fast_info_missing():
     provider = YahooQuoteProvider(
-        ticker_factory=lambda _: _FakeTicker(fast_info={}, info={"regularMarketPrice": 99.5})
+        ticker_factory=lambda _: build_fake_yahoo_ticker(fast_info={}, info={"regularMarketPrice": 99.5})
     )
 
     price = provider.get_limit_reference_price("AAPL", "BUY")
@@ -44,7 +38,7 @@ def test_yahoo_quote_provider_falls_back_to_info_when_fast_info_missing():
 
 def test_yahoo_quote_provider_returns_none_when_no_price_candidates_available():
     provider = YahooQuoteProvider(
-        ticker_factory=lambda _: _FakeTicker(fast_info={}, info={})
+        ticker_factory=lambda _: build_fake_yahoo_ticker(fast_info={}, info={})
     )
 
     price = provider.get_limit_reference_price("AAPL", "BUY")
@@ -54,7 +48,7 @@ def test_yahoo_quote_provider_returns_none_when_no_price_candidates_available():
 
 def test_yahoo_quote_provider_rejects_unknown_side():
     provider = YahooQuoteProvider(
-        ticker_factory=lambda _: _FakeTicker(fast_info={"ask": 101.25})
+        ticker_factory=lambda _: build_fake_yahoo_ticker(fast_info={"ask": 101.25})
     )
 
     with pytest.raises(ValueError, match="Unsupported side for quote lookup"):
